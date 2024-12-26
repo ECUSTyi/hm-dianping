d17bf7d62fb6a52ddcb7643b625ad4ef19189c34
zhongyi <1826600295@qq.com>
2024-12-26 19:36:23 +0800

    解决优惠券下单库存超卖问题，实现了一人下单一张优惠券的功能

--- src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
@@ -45,13 +45,13 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
     @Override
     public Result queryById(Long id) {
         // 缓存穿透
-        //Shop shop1 = cacheClient.queryWithPassThrough(CACHE_SHOP_KEY,id,Shop.class, this::getById, CACHE_SHOP_TTL, TimeUnit.MINUTES);
+        Shop shop = cacheClient.queryWithPassThrough(CACHE_SHOP_KEY,id,Shop.class, this::getById, CACHE_SHOP_TTL, TimeUnit.MINUTES);
 
         // 互斥锁解决缓存击穿
         //Shop shop2 = queryWithMutex(id);
 
         // 逻辑超时时间解决缓存击穿
-        Shop shop = cacheClient.queryWithLogicalExpire(CACHE_SHOP_KEY,id, Shop.class,this::getById, 20L, TimeUnit.SECONDS);
+//        Shop shop = cacheClient.queryWithLogicalExpire(CACHE_SHOP_KEY,id, Shop.class,this::getById, 30L, TimeUnit.MINUTES);
         if (shop == null) {
             return Result.fail("店铺不存在");
         }
